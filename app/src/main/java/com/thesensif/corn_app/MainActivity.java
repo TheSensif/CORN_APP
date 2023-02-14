bcceee78c18cabe511c5c1739777db535b013597
TheSensif <sergisimon7@gmail.com>
2023-02-14 18:26:50 +0100

    feat/CORN-5: create menu and fracments

--- app/src/main/java/com/thesensif/corn_app/MainActivity.java
+++ app/src/main/java/com/thesensif/corn_app/MainActivity.java
@@ -1,14 +1,54 @@
 package com.thesensif.corn_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.thesensif.corn_app.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new InicioFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()) {
+                case R.id.Inici:
+                    replaceFragment(new InicioFragment());
+                    break;
+                case R.id.Cobrament:
+                    replaceFragment(new CobramentFragment());
+                    break;
+                case R.id.Escanejar:
+                    replaceFragment(new EscanejarFragment());
+                    break;
+                case R.id.Historial:
+                    replaceFragment(new HistorialFragment());
+                    break;
+                case R.id.Perfil:
+                    replaceFragment(new PerfilFragment());
+                    break;
+            }
+
+            return true;
+        });
     }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+    }
+
 }
