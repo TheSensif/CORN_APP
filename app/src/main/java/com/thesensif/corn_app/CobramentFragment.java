824d15959047de8e63634f1c07d3626fd3fc1ae2
SergioHerreraMarin <sergioherreramarin@gmail.com>
2023-02-27 20:05:10 +0100

    generateQR done 2

--- app/src/main/java/com/thesensif/corn_app/CobramentFragment.java
+++ app/src/main/java/com/thesensif/corn_app/CobramentFragment.java
@@ -1,12 +1,34 @@
 package com.thesensif.corn_app;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.text.InputType;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.zxing.WriterException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +45,10 @@ public class CobramentFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private String token = "";
+    private ImageView qrImage;
+    private Button generateTokenButton;
 
     public CobramentFragment() {
         // Required empty public constructor
@@ -55,10 +81,126 @@ public class CobramentFragment extends Fragment {
         }
     }
 
+
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_cobrament, container, false);
+        Bitmap bitmap;
+
+        qrImage = v.findViewById(R.id.imageQr);
+        generateTokenButton = v.findViewById(R.id.buttonGenerateToken);
+
+
+        generateTokenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkDB();
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cobrament, container, false);
+        return v;
     }
+
+    private void generateQE(){
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Bitmap bitmap;
+                // Initializing the QR Encoder with your value to be encoded, type you required and Dimension
+                QRGEncoder qrgEncoder = new QRGEncoder(token, null, QRGContents.Type.TEXT, 100);
+                qrgEncoder.setColorBlack(Color.BLACK);
+                qrgEncoder.setColorWhite(Color.WHITE);
+                try {
+                    // Getting QR-Code as Bitmap
+                    bitmap = qrgEncoder.getBitmap(0);
+                    // Setting Bitmap to ImageView
+                    qrImage.setImageBitmap(bitmap);
+                } catch (Exception e) {
+                    System.out.println("error");
+                }
+            }
+    });
+    }
+
+
+    private void checkDB(){
+        final Activity activity = getActivity();
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("Introducir cantidad");
+
+                final EditText input = new EditText(getActivity());
+
+                input.setInputType(InputType.TYPE_CLASS_NUMBER);
+                builder.setView(input);
+
+                // Set up the buttons
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String cantidad = input.getText().toString();
+                        try {
+                            JSONObject obj = new JSONObject("{}");
+
+                            obj.put("user_id", MainActivity.telephon);
+                            obj.put("amount", cantidad);
+                            UtilsHTTP.sendPOST("https://cornapi-production-5680.up.railway.app:443/api/setup_payment", obj.toString(), (response) -> {
+                                try {
+                                    JSONObject obj2 = new JSONObject(response);
+                                    if (obj2.getString("status").equals("OK")) {
+                                        token = obj2.getString("transaction_token");
+                                        generateQE();
+                                    } else if (obj2.getString("status").equals("ERROR")) {
+                                        Handler handler = new Handler(Looper.getMainLooper());
+                                        handler.post(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                                                try {
+                                                    builder.setTitle("Error")
+                                                            .setMessage(obj2.getString("message"))
+                                                            .setNegativeButton("OK", new DialogInterface.OnClickListener() {
+                                                                public void onClick(DialogInterface dialog, int id) {
+                                                                    // User clicked OK button
+                                                                }
+                                                            });
+                                                } catch (JSONException e) {
+                                                    e.printStackTrace();
+                                                }
+                                                AlertDialog dialog2 = builder.create();
+                                                dialog2.show();
+                                            }
+                                        });
+                                    }
+                                } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                            });
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                builder.show();
+            }
+        });
+    }
+
+
 }
