41908679dce91e496fe8ef59b48663c8ebd5c8ac
TheSensif <sergisimon7@gmail.com>
2023-03-02 16:34:06 +0100

    feat/CORN-24: create interface

--- app/src/main/java/com/thesensif/corn_app/RegisterActivity.java
+++ app/src/main/java/com/thesensif/corn_app/RegisterActivity.java
@@ -2,7 +2,10 @@ package com.thesensif.corn_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        loginButton();
+    }
+
+    private void loginButton() {
+        Button registerButon = findViewById(R.id.loginRegister);
+        registerButon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+            }
+        });
     }
 }
