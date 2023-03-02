3fbf42ab233b26135a9770cf7bb773b2ea5986a9
TheSensif <sergisimon7@gmail.com>
2023-03-02 15:58:10 +0100

    feat/CORN-23: add LoginActivity & link to RegisterActivity

---
+++ app/src/main/java/com/thesensif/corn_app/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.thesensif.corn_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        registerButton();
+    }
+
+    private void registerButton() {
+        Button registerButon = findViewById(R.id.register);
+        registerButon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
+    }
+}
